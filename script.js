e3931df45b53f69c879e52dc6e02a7fd03bac862
Marsleyy <44040638+Marsleyy@users.noreply.github.com>
2018-12-12 17:52:55 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".B1").click(function() {
+    $(".MoveOne").slideToggle();
+});
+$(".B2").click(function() {
+    $(".MoveTwo").fadeToggle();
+});
+$(".B3").click(function() {
+    $(".MoveThree").toggle();
+});
